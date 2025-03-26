ada83fdb0df18a183ca9c133c083a24a2ec4a9bb
OM3X4 <om3x4e@gmail.com>
2025-03-26 14:28:16 +0200

    26/3

--- core/views.py
+++ core/views.py
@@ -9,7 +9,6 @@ from django.contrib.auth.hashers import make_password
 from django.contrib.auth.models import User
 from google.oauth2 import id_token
 from google.auth.transport import requests
-import os
 
 googleClientId = "441640127193-68e3tk5smeclt6esno0s311ptlrb1hs1.apps.googleusercontent.com"
 
@@ -28,7 +27,11 @@ def tasksCreateList(request):
             if topic and topic.user != request.user:
                 return Response({"message" : "This Is Not Your Habit"} , status=status.HTTP_403_FORBIDDEN)
             serial.save(user=request.user)
-            return Response(status=status.HTTP_201_CREATED)
+
+            tasks = Task.objects.filter(user=request.user).select_related("topic" , "user")
+            serial = TaskSerializer(tasks , many=True)
+
+            return Response(serial.data  ,status=status.HTTP_201_CREATED)
         return Response(serial.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(["GET" , "POST"])
@@ -99,14 +102,29 @@ def register(request):
 @api_view(["POST"])
 def googleAuth(request):
     token = request.data.get("token")
+
+    if not token:
+        return Response({"message" : "Missing Token"} , status=status.HTTP_400_BAD_REQUEST)
+
+
     try:
         google_user = id_token.verify_oauth2_token(token , requests.Request() ,googleClientId )
 
-        user, created = User.objects.get_or_create(email=google_user["email"] , defaults={"username": google_user["email"]})
+        user, created = User.objects.get_or_create(
+                                            email=google_user["email"],
+                                            defaults={
+                                                "username": google_user["email"],
+                                                "first_name": google_user.get("given_name", ""),
+                                                "last_name": google_user.get("family_name", ""),
+                                            }
+                                        )
 
         refresh = RefreshToken.for_user(user)
         return Response({"access" :str(refresh.access_token) , "refresh": str(refresh)})
 
 
-    except Exception as e:
+    except ValueError:
         return Response({"error": "Invalid token"}, status=400)
+
+    except Exception as e:
+        return Response({"error": str(e)}, status=500)
